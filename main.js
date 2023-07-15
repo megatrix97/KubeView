3e60ac8a9525d9528e86f14dcccda4649a679eed
nithin banka <nithinbanka@gmail.com>
2023-07-16 04:00:28 +0530

    inital commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    window = new BrowserWindow({
+        width: 500,
+        height: 500
+    })
+    
+    window.loadFile('start-screen.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform != "darwin") app.quit()
+})
