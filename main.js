319c9cbf1f74234fb9878d577ea0031c4fc0cc3d
nithin banka <nithinbanka@gmail.com>
2023-07-23 00:28:15 +0530

    initial ui changes

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
     window = new BrowserWindow({
         width: 500,
-        height: 500
+        height: 500,
+        autoHideMenuBar: true
     })
     
     window.loadFile('start-screen.html')
