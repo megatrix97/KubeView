21a35b9785c7b13822485b64082c14b39f9728c5
nithin banka <nithinbanka@gmail.com>
2023-07-24 16:37:18 +0530

    added search box

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
 const { app, BrowserWindow } = require('electron')
+const { ipcMain } = require('electron')
+
+
+let window
 
 const createWindow = () => {
     window = new BrowserWindow({
         width: 500,
         height: 500,
-        autoHideMenuBar: true
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
     })
     
     window.loadFile('start-screen.html')
 }
+
+let shiftPressCount = 0
+let shiftPressTimer = null
+
+ipcMain.on('ctrlPressed', () => {
+    shiftPressCount += 1
+    if (shiftPressCount === 1) {
+        shiftPressTimer = setTimeout(() => {
+            shiftPressCount = 0
+        }, 500)
+    } else if (shiftPressCount === 2) {
+        clearTimeout(shiftPressTimer)
+        shiftPressCount = 0
+        window.webContents.send('toggleInputBoxVisibility')
+    }
+})
 
 app.whenReady().then(() => {
     createWindow()
